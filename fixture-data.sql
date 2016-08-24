8e0b82245e6812a1f2a5c88e221bd4aa46ccae12
Aileen Santos <asantos3026@gmail.com>
2016-08-24 13:10:22 -0700

    WIP

--- fixture-data.sql
+++ fixture-data.sql
@@ -7,14 +7,14 @@ VALUES
 ;
 
 INSERT INTO
-  todos (user_id, title, work, created_at)
+  todos (user_id, title, work, completed, created_at)
 VALUES
-  (1, 'eat some ice cream', true, now()),
-  (1, 'drop some knowledge', true, now()),
-  (1, 'eat some cake', false, now()),
-  (1, 'sleep zzZZzz', false, now()),
-  (2, 'pat jared on the head', true, now()),
-  (2, 'read 1000 lines of code', true, now()),
-  (2, 'feed cat', false, now()),
-  (2, 'sleep 8 hours', false, now())
+  (1, 'eat some ice cream', true, true, now()),
+  (1, 'drop some knowledge', true, false, now()),
+  (1, 'eat some cake', false, true, now()),
+  (1, 'sleep zzZZzz', false, false, now()),
+  (2, 'pat jared on the head', true, false, now()),
+  (2, 'read 1000 lines of code', true, true, now()),
+  (2, 'feed cat', false, true, now()),
+  (2, 'sleep 8 hours', false, false, now())
 ;
