3de6cf5270d60194895e97b57f809315c7718c60
Aileen Santos <asantos3026@gmail.com>
2016-08-27 11:49:50 -0700

    fixed sign up

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  email VARCHAR(20) NOT NULL,
+  email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   avatar_url VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL
