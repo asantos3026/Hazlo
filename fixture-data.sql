abd1a98b0097937890e3a6f454b80c32188a4b8a
Aileen Santos <asantos3026@gmail.com>
2016-08-27 22:21:54 -0700

    fixed creating a new todo

--- fixture-data.sql
+++ fixture-data.sql
@@ -1,20 +1,26 @@
-INSERT INTO
-  users (id, email, password, avatar_url, created_at)
-VALUES
-  (1, 'jared@example.org', 'cheese', '', now()),
-  (2, 'ana@example.org', 'hats', '', now()),
-  (3, 'thomas@example.org', 'dancing', '', now())
-;
+-- INSERT INTO
+--   users (email, password, avatar_url, created_at)
+-- VALUES
+--   (1, 'jared@example.org', 'cheese', '', now()),
+--   (2, 'ana@example.org', 'hats', '', now()),
+--   (3, 'thomas@example.org', 'dancing', '', now())
+-- ;
 
 INSERT INTO
   todos (user_id, title, work, completed, created_at)
 VALUES
-  (1, 'eat some ice cream', true, true, now()),
-  (1, 'drop some knowledge', true, false, now()),
-  (1, 'eat some cake', false, true, now()),
-  (1, 'sleep zzZZzz', false, false, now()),
-  (2, 'pat jared on the head', true, false, now()),
+  (1, 'study routes express', true, false, now()),
+  (1, 'write act 3 of screenplay', true, false, now()),
+  (1, 'write a route to create a new todo', true, false, now()),
+  (1, 'get some rest', false, false, now()),
+  (1, 'study routes express', true, false, now()),
+  (1, 'write act 3 of screenplay', true, false, now()),
+  (1, 'write a route to create a new todo', true, false, now()),
+  (1, 'study routes express', true, false, now()),
+  (1, 'write act 3 of screenplay', true, false, now()),
+  (1, 'write a route to create a new todo', true, false, now()),
+  (2, 'go grocery shopping', true, false, now()),
   (2, 'read 1000 lines of code', true, true, now()),
-  (2, 'feed cat', false, true, now()),
-  (2, 'sleep 8 hours', false, false, now())
+  (2, 'buy incense for altar', false, true, now()),
+  (2, 'return book to Berkeley public library', false, false, now())
 ;
